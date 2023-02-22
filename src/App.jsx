9e3c6649ccdc7aac082c70fa160f0656f9d18654
Marcin Dziedzinski <m.m.dziedzinski@gmail.com>
2023-02-22 22:18:43 +0100

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import axios from "axios";
+
+const client = "https://63f67f8a59c944921f752b54.mockapi.io/api/users";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState("");
+  const [userName, setUserName] = useState("");
+  const [title, setTitle] = useState("");
 
+  const fetchData = () => {
+    fetch(client)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(client, {
+      method: "POST",
+      body: JSON.stringify({
+        name: userName,
+        userId: Math.random().toString(36).slice(2),
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => res.json())
+      .then((title) => {
+        setUsers((user) => [title, ...users]);
+        setTitle("");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  console.log(users);
   return (
     <div className="App">
       <div>
@@ -16,9 +62,20 @@ function App() {
         </a>
       </div>
       <h1>People</h1>
-   
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Name</p>
+          <input
+            type="text"
+            name="title"
+            value={userName}
+            onChange={handleChange}
+          ></input>
+        </label>
+        <button type="submit">Create</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
