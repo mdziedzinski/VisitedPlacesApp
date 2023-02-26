5a2367176d5f9214e1c3a0d7bcdf8fc6b021974b
Marcin Dziedzinski <m.m.dziedzinski@gmail.com>
2023-02-26 22:43:41 +0100

    unsplash api

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,28 @@ import "./App.css";
 import axios from "axios";
 import PlacesList from "./components/PlacesList";
 import Box from "@mui/material/Box";
+import { createApi } from "unsplash-js";
 
 const client = "https://63f67f8a59c944921f752b54.mockapi.io/api/users";
 
 function App() {
   const [places, setPlaces] = useState("");
-  const [photos, setPhotos] = useState(null);
-  const [cityState, setCityState] = useState("Gniezno");
 
-  console.log(photos);
+  useEffect(() => {
+    fetchClient();
+  }, []);
+
+  const fetchClient = async () => {
+    const data = await fetch(
+      `https://63f67f8a59c944921f752b54.mockapi.io/api/users`
+    );
+    const dataRes = await data.json();
+
+    setPlaces(dataRes);
+  };
+
+  console.log(places);
+  // console.log(photo);
 
   return (
     <Box>
--- src/components/PlacesListItem.jsx
+++ src/components/PlacesListItem.jsx
@@ -5,23 +5,37 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useState, useEffect } from "react";
 
 const PlacesListItem = ({ place }) => {
   const date = new Date(place.date);
+  const photoQuery = place.city;
+  const [photo, setPhoto] = useState("");
 
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+  const fetchRequest = async () => {
+    const data = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&photos?&query=${photoQuery}&client_id=${
+        import.meta.env.VITE_REACT_APP_UNSPLASH_API_KEY
+      }&per_page=1`
+    );
+    const dataJ = await data.json();
+    const result = dataJ.results;
+    const smallPhoto = result[0].urls.small;
+
+    setPhoto(smallPhoto);
   };
+
+  useEffect(() => {
+    fetchRequest();
+  }, []);
+
   return (
     <Card>
       <CardMedia
         component="img"
         alt={place.note}
         height="140"
-        image={place.photo}
+        image={photo ? photo : place.photo}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
