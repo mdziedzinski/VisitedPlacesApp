2e172d35b983b5517f32d9d4b49ef83c65cb2c0e
Marcin Dziedzinski <m.m.dziedzinski@gmail.com>
2023-03-04 18:22:44 +0100

    rendering from api

--- src/components/PlacesListItem.jsx
+++ src/components/PlacesListItem.jsx
@@ -19,7 +19,7 @@ const PlacesListItem = ({ place }) => {
 
   const getImages = async () => {
     const response = await axios
-      .get(`https://api.unsplash.com/photos/random`, {
+      .get(`https://api.unsplash.com/search/photos`, {
         headers: {
           Authorization: `Client-ID ${
             import.meta.env.VITE_REACT_APP_UNSPLASH_API_KEY
@@ -28,15 +28,16 @@ const PlacesListItem = ({ place }) => {
         params: {
           query: `${photoQuery} ? ${photoQuery} : 'city'`,
           count: 1,
+          per_page: 1,
+          page: 1,
         },
       })
       .catch(function (error) {
         console.log(error.response);
       });
 
-    setPhoto(response.data[0]);
-
-    console.log(response.data[0]);
+    setPhoto(response.data.results[0]);
+    console.log(response.data.results[0]);
     return response;
   };
 
@@ -55,48 +56,45 @@ const PlacesListItem = ({ place }) => {
   // };
 
   useEffect(() => {
-    // fetchRequest();
     getImages();
   }, [photoQuery]);
 
-  const contentCard = (
-    <Card>
-      <CardMedia
-        component="img"
-        alt={place.note}
-        height="140"
-        image={photo ? photo.urls.small : place.photo}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {place.city}, {place.country}
-        </Typography>
-        <Typography variant="overline" display="block" gutterBottom>
-          visited on {date.toLocaleDateString()}
-        </Typography>
-        <Typography variant="overline" display="block" gutterBottom>
-          Photo by
-          <Link href={photo ? photo.user.links.html : "test"}>
-            {photo ? photo.user.first_name : "test"}
-          </Link>
-          on
-          <Link href="https://unsplash.com/"> Unsplash</Link>
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {place.note}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Delete</Button>
-        <Button size="small">Edit</Button>
-      </CardActions>
-    </Card>
-  );
+  console.log(photo);
 
   return (
     <>
       {photo ? (
-        contentCard
+        <Card>
+          <CardMedia
+            component="img"
+            alt={place.note}
+            height="140"
+            image={photo ? photo.urls.small : place.photo}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {place.city}, {place.country}
+            </Typography>
+            <Typography variant="overline" display="block" gutterBottom>
+              visited on {date.toLocaleDateString()}
+            </Typography>
+            <Typography variant="overline" display="block" gutterBottom>
+              Photo by{" "}
+              <Link href={photo ? photo.user.links.html : "Loading"}>
+                {photo ? photo.user.first_name : "Loading"}
+              </Link>
+              {' '}on
+              <Link href="https://unsplash.com/"> Unsplash</Link>
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {place.note}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Delete</Button>
+            <Button size="small">Edit</Button>
+          </CardActions>
+        </Card>
       ) : (
         <>
           <Skeleton variant="rectangular" height="140px" width="100%" />
